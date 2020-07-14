1eb7204c42a9765325495c5ac53a42d006962791
suhyun <ksuh0805@ajou.ac.kr>
2020-07-14 23:11:38 +0900

    spring boot init

---
+++ lol-record-search/src/main/java/org/ajou/realcoding8/lolrecordsearch/LolRecordSearchApplication.java
@@ -0,0 +1,13 @@
+package org.ajou.realcoding8.lolrecordsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LolRecordSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LolRecordSearchApplication.class, args);
+	}
+
+}
---
+++ lol-record-search/src/test/java/org/ajou/realcoding8/lolrecordsearch/LolRecordSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ajou.realcoding8.lolrecordsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LolRecordSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
