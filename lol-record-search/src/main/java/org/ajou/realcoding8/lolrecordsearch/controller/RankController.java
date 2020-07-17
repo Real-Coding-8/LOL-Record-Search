61eafd068f23122f69b2c08201e77ae69c5e1372
suhyun <ksuh0805@ajou.ac.kr>
2020-07-18 02:32:19 +0900

    Add Step3 solo rank search

---
+++ lol-record-search/src/main/java/org/ajou/realcoding8/lolrecordsearch/controller/RankController.java
@@ -0,0 +1,22 @@
+package org.ajou.realcoding8.lolrecordsearch.controller;
+
+import org.ajou.realcoding8.lolrecordsearch.domain.Rank;
+import org.ajou.realcoding8.lolrecordsearch.service.RankService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+public class RankController {
+
+    @Autowired
+    private RankService rankService;
+
+    @GetMapping("/rank-service/rank")
+    public Rank getRank(@RequestParam String encryptedSummonerId) {
+        return rankService.getSoloRank(encryptedSummonerId);
+    }
+}
